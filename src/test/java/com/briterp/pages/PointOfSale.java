9851989d2b4160c5d7bcef68f9a20050168c6f06
ahmetbalci <ahmetbalci317@Gmail.com>
2019-02-03 22:56:11 -0500

    3 test cases

--- src/test/java/com/briterp/pages/PointOfSale.java
+++ src/test/java/com/briterp/pages/PointOfSale.java
@@ -18,5 +18,39 @@ public class PointOfSale {
     @FindBy (xpath = "//*[@class='ui-sortable']")
     public WebElement table;
 
+    @FindBy (xpath = "//div[@class='o_sub_menu_content']//div[8]//ul[3]//li[1]//a[1]//span[1]")
+    public WebElement orders;
+
+    @FindBy(xpath = "//button[@class='btn btn-primary btn-sm dropdown-toggle']")
+    public WebElement measures;
+
+    @FindBy(xpath = "//a[contains(text(),'Total Price')]")
+    public  WebElement totalPrice;
+
+    @FindBy(xpath = "//a[contains(text(),'Subtotal w/o discount')]")
+    public  WebElement subTotalDiscount;
+
+    @FindBy(xpath = "//a[contains(text(),'Total Discount')]")
+    public WebElement totalDiscount;
+
+    @FindBy(xpath = "//a[contains(text(),'Average Price')]")
+    public WebElement averagePrice;
+
+    @FindBy(xpath = "//a[contains(text(),'# of Lines')]")
+    public WebElement ofLines;
+
+    @FindBy(xpath = "//a[contains(text(),'Product Quantity')]")
+
+    public WebElement productQuantity;
+
+    @FindBy(xpath = "//a[contains(text(),'Delay Validation')]")
+    public WebElement delayValidation;
+
+    @FindBy(xpath = "//a[contains(text(),'Count')]")
+    public WebElement count;
+
+    @FindBy(xpath = "//button[@class='btn btn-default fa fa-bar-chart-o o_graph_button active']")
+    public WebElement barChart;
+
 
 }
